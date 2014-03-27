b3b4c3bc88aa069e981ee33db0b7e61eb1dda3de
Dmytro Chernetsov <fonque@gmail.com>
2014-03-27 19:44:00 +0200

    first view and first urls changes

--- rango/urls.py
+++ rango/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from rangoapp import views
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -6,7 +7,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'rango.views.home', name='home'),
+    url(r'^$', 'rangoapp.views.index', name='index'),
     # url(r'^rango/', include('rango.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
